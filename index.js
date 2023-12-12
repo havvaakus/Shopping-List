d49b2b3490823c4dae479a0e8b9a2a0aa79ab466
Havva Kuş <havvaakus@gmail.com>
2023-12-12 20:44:22 +0300

    CSS Rev

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
-addButtonEl.addEventListener("click", function() {
+addButtonEl.addEventListener("click", function() {       
     let inputValue = inputFieldEl.value
     push(shoppingListInDB, inputValue)
     clearInputFieldEl()
